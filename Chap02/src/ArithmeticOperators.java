ee85fdae98f412cad83d86b06a4949dbbafe2a7b
In-Kwon Lee <iklee@yonsei.ac.kr>
2024-08-27 01:31:23 +0900

    Chap2 - ArithmeticOperators

--- Chap02/src/ArithmeticOperators.java
+++ Chap02/src/ArithmeticOperators.java
@@ -1,13 +1,23 @@
 public class ArithmeticOperators {
     public static void main (String[] args) {
-        int a = 10, b = 5;
-        int sum = a + b;       // 15
-        int difference = a - b; // 5
-        int product = a * b;    // 50
+        int a = 11, b = 5;
+        int sum = a + b;       // 16
+        int difference = a - b; // 6
+        int product = a * b;    // 55
         int quotient = a / b;   // 2
-        int remainder = a % b;  // 0
-
-        a++; // 11
-        b--; // 4
+        int remainder = a % b;  // 1
+        int c = a++; // 11
+        int d = ++a; // 13
+        int e = b--; // 5
+        int f = --b; // 3
+        System.out.println("sum = " + sum + "\n"+
+                "difference = " + difference + "\n" +
+                "product = " + product + "\n" +
+                "quotient = " + quotient + "\n" +
+                "remainder = " + remainder + "\n" +
+                "c = " + c + "\n" +
+                "d = " + d + "\n" +
+                "e = " + e + "\n" +
+                "f = " + f);
     }
 }
