2554a776dff52b0eb2f754156105d39913684b97
In-Kwon Lee <iklee@yonsei.ac.kr>
2024-09-01 01:21:17 +0900

    240901

--- Chap03/src/MultiDArray1.java
+++ Chap03/src/MultiDArray1.java
@@ -1,27 +1,27 @@
 public class MultiDArray1 {
     public static void main(String[] argv) {
-        int[][] a1 = new int[2][3];
-        int[][] a2 = {{1, 2, 3, 4}, {5, 6}, {7, 8, 9}};
+        int[][] array1 = new int[2][3];
+        int[][] array2 = {{1, 2, 3, 4}, {5, 6}, {7, 8, 9}};
 
-        for (int i = 0; i < a1.length; i++) // fill the array a1
-            for (int j = 0; j < a1[i].length; j++) {
-                a1[i][j] = i + j;
+        for (int i = 0; i < array1.length; i++) // fill the array a1
+            for (int j = 0; j < array1[i].length; j++) {
+                array1[i][j] = i + j;
             }
 
-        System.out.println("----- a1 -------");
+        System.out.println("----- array1 -------");
 
-        for (int i = 0; i < a1.length; i++) {  // print the array a1
-            for (int j = 0; j < a1[i].length; j++) {
-                System.out.print(a1[i][j] + " ");
+        for (int i = 0; i < array1.length; i++) {  // print the array a1
+            for (int j = 0; j < array1[i].length; j++) {
+                System.out.print(array1[i][j] + " ");
             }
             System.out.println();
         }
 
-        System.out.println("----- a2 -------");
+        System.out.println("----- array2 -------");
 
-        for (int i = 0; i < a2.length; i++) {  // print the array a2
-            for (int j = 0; j < a2[i].length; j++) {
-                System.out.print(a2[i][j] + " ");
+        for (int i = 0; i < array2.length; i++) {  // print the array a2
+            for (int j = 0; j < array2[i].length; j++) {
+                System.out.print(array2[i][j] + " ");
             }
             System.out.println();
         }
